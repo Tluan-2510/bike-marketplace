d8b2617189c44d0daada8c15da353d3cd4f81fb2
Caesar <monalisarz2934@gmail.com>
2026-04-07 22:59:13 +0700

    function updates

--- backend/app/helpers/functions.php
+++ backend/app/helpers/functions.php
@@ -38,22 +38,4 @@ function app($abstract = null)
     return $container->make($abstract);
 }
 
-function response()
-{
-    return new \App\Http\ResponseFactory();
-}
 
-function env(string $key, $default = null)
-{
-    return $_ENV[$key] ?? $_SERVER[$key] ?? $default;
-}
-
-function now()
-{
-    return Carbon::now();
-}
-
-function db()
-{
-    return \Illuminate\Database\Capsule\Manager::connection();
-}
